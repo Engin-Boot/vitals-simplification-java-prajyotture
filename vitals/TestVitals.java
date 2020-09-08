431a7c3458b2857cfd7de6cfe60a2c2ed92ba4e3
Prajyot Ture <47532830+prajyotture@users.noreply.github.com>
2020-09-08 20:53:48 +0530

    Update TestVitals.java

--- vitals/TestVitals.java
+++ vitals/TestVitals.java
@@ -1,26 +1,32 @@
 package vitals;
-public class TestVitals {
-	 public  boolean vitalsAreOk(int bpm, int spo2, int respRate) {
-
+public class TestVitals implements AlertVitals{
+	
+	public  boolean vitalsAreOk(int bpm, int spo2, int respRate) {
+		 	System.out.println("Hello");
 	        boolean bpmVal=bpmChecker(bpm);
 	        boolean spo2Val=spo2Checker(spo2);
 	        boolean respVal=respChecker(respRate);
-	        System.out.println();
+	        System.out.println(bpmVal);
 			return bpmVal && spo2Val && respVal;
 
 	    }
-
-	private boolean bpmChecker(int bpm) {
+     
+	public boolean bpmChecker(int bpm) {
 		if (bpm<70) {
+			alertVital("bpm",bpm);
 			return false;
+			
 		}
 		else if(bpm>150) {
+			alertVital("bpm",bpm);
 		return false;
+		
 	}
 		return true;
 	}
 	private boolean spo2Checker(int spo2) {
 		if(spo2<90) {
+			alertVital("spo2",spo2);
 		return false;
 	}
 		return true;
@@ -28,15 +34,22 @@ public class TestVitals {
 	
 	private boolean respChecker(int respRate) {
 		if (respRate<30) {
+			alertVital("respRate",respRate);
 			return false;
 		}
 		else if(respRate>95) {
+			alertVital("respRate",respRate);
 		return false;
 	}
 		return true;
 	}
 
-	
+	@Override
+	public void alertVital(String vital,int val) {
+		
+		System.out.println("The vital " +vital+  "  has high value that is "+val);
+	}
 
+	
 
 }
